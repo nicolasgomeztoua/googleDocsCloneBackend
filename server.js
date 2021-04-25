9f7e439c1f925acd5c022045498c662ce494224b
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-25 19:02:04 +0200

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const express = require("express");
 const mongoose = require("mongoose");
 const Document = require("./Document");
 require("dotenv").config({ path: "./config.env" });
+
+const app = express();
+
 mongoose.connect(process.env.DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -40,3 +44,8 @@ async function findOrCreateDocument(id) {
   if (document) return document;
   return await Document.create({ _id: id, data: defaultValue });
 }
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
